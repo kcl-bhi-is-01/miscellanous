cae764a2cca8e634f8cbf9fe0f40cc5040babc04
kcl-bhi-is-01 <108937001+kcl-bhi-is-01@users.noreply.github.com>
2022-08-01 15:02:20 +0100

    Add files via upload

---
+++ MySQL_DML_students_v01.sql
@@ -0,0 +1,19 @@
+--
+-- Student stuff DML script
+--
+-- NB _dt is a date time string in R format "2022-07-29 06:04:23"
+--
+-- v01 - 1st cut
+--
+use IS_students_v01;
+--
+--
+insert into students (moodle_user_id, moodle_user_name, moodle_full_name, moodle_email)
+values (28281, "k3232322", "Nigel Earplugs", "nigel.earplugs@hotmail.com");
+--
+insert into students (moodle_user_id, moodle_user_name, moodle_full_name, moodle_email)
+values (9888, "k1010101", "Andrew Pandy", "andy.pandy@teddy.com");
+--
+insert into students (moodle_user_id, moodle_user_name, moodle_full_name, moodle_email)
+values (58778, "k6767677", "Loopiella Lou", "loopy.lou@andy.com");
+--
